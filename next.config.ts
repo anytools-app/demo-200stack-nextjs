f96df9e0515f31b94369df3a67aa0bd8180660ab
Yujiro Arai <yujiroarai@gmail.com>
2025-11-07 15:16:35 +0900

    configure static export and microCMS image settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // Imageコンポーネントで表示する画像をホストしているサーバーのURL（microCMSを使う場合↓）
+    domains: ["images.microcms-assets.io"],
+    // 画像の最適化処理を動かさない
+    unoptimized: true,
+  },
+  // export 時に /path/index.html 形式で出力する
+  trailingSlash: true,
+
+  // 静的出力
+  output: "export",
 };
 
 export default nextConfig;
